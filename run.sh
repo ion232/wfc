858a56795511f780b699edd1c8c092f30b5c3269
ion232 <66679118+ion232@users.noreply.github.com>
2024-04-09 10:52:59 +0100

    test: add id_set tests

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ main() {
     ninja -C build
     # export ASAN_OPTIONS=halt_on_error=1:symbolize=1:print_stacktrace=1:check_initialization_order=1
     # export UBSAN_OPTIONS=print_stacktrace=1
-    ./build/wfc
+    ./build/wfc_unit_tests
     # lldb -o run ./build/wfc
 }
 
