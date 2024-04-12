cb6074b80ca616deb0d6540bd46dd53a67899e5a
ion232 <66679118+ion232@users.noreply.github.com>
2024-04-12 14:46:09 +0100

    refactor: use IdMap for weights

--- src/heuristic/weights.cc
+++ src/heuristic/weights.cc
@@ -2,7 +2,7 @@
 
 namespace wfc::heuristic {
 
-Weights::Weights(std::unordered_map<Id, std::size_t>&& weights)
+Weights::Weights(IdMap<std::size_t>&& weights)
     : m_weights(std::move(weights))
 {}
 
