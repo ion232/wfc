a1454f8053a66a55641c605a8f34c32b7f438597
ion232 <66679118+ion232@users.noreply.github.com>
2024-04-23 12:39:32 +0100

    refactor: change benchmark output and add 512x512

--- app/benchmark.cc
+++ app/benchmark.cc
@@ -16,11 +16,10 @@ void Benchmark::run(const std::vector<Parameters>& parameters_list) {
     std::cout << "Image: " << m_wfc_factory.image_path() << std::endl;
 
     for (const auto& parameters : parameters_list) {
-        std::cout << "Width: " << parameters.width << " ";
-        std::cout << "Height: " << parameters.height << " ";
+        std::cout << parameters.width << "x" << parameters.height << " ";
 
         if (auto elapsed = solve_time(parameters)) {
-            std::cout << "Elapsed: " << std::to_string(elapsed->count());
+            std::cout << "t=" << std::to_string(elapsed->count()) << "ms";
         } else {
             // ion232: This could be replaced with more granular data.
             std::cout << "Failed.";
